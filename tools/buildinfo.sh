8f2b3edf1d176b3dc45d789bc5427f3bd50e556d
bauner <bauner@gmx.de>
2017-02-15 16:32:40 +0100

    add ro.ota.version

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -31,6 +31,10 @@ echo "ro.product.brand=$PRODUCT_BRAND"
 echo "ro.product.name=$PRODUCT_NAME"
 echo "ro.product.device=$TARGET_DEVICE"
 
+echo "# version for OTA update"
+echo "ro.ota.version=`date +%Y%m%d`"
+
+
 # These values are deprecated, use "ro.product.cpu.abilist"
 # instead (see below).
 echo "# ro.product.cpu.abi and ro.product.cpu.abi2 are obsolete,"
